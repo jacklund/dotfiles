1358a127649ecd9244c67241469bbaa64305b545
Jack Lund <jackl@geekheads.net>
2019-08-18 22:35:10 -0500

    Replace Makefile with script

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+TOP_LEVEL_FILES="aliases bashrc fonts fzf.bash fzf.zsh gitconfig gorc tmux.conf vimrc zshrc zshrc.d"
+
+for file in ${TOP_LEVEL_FILES}; do
+  if [[ $(readlink -f ~/.${file}) != ${PWD}/${file} ]]; then
+    [[ -e ~/.${file} ]] && mv ~/.${file} ~/.${file}.orig
+    ln -s ${PWD}/${file} ~/.${file}
+  fi
+done
+
+for file in config/*/*; do
+  if [[ $(readlink -f ~/.${file}) != ${PWD}/${file} ]]; then
+    if [[ -e ~/.${file} ]]; then
+      mv ~/.${file} ~/.${file}.orig
+    else
+      mkdir -p $(dirname ~/.${file})
+    fi
+    ln -s ${PWD}/${file} ~/.${file}
+  fi
+done
+
+mkdir -p ~/bin
+for file in bin/*; do
+  if [[ $(readlink -f ~/${file}) != ${PWD}/${file} ]]; then
+    [[ -e ~/.${file} ]] && mv ~/${file} ~/${file}.orig
+    ln -s ${PWD}/${file} ~/${file}
+  fi
+done
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
